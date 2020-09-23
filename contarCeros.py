7bddd976d6d8baa755db685993382013e1cd7afe
Gerson2102 <71728860+Gerson2102@users.noreply.github.com>
2020-09-22 21:37:33 -0600

    Archivo python
    Función que cuenta los ceros de un número

---
+++ contarCeros.py
@@ -0,0 +1,26 @@
+# Objetivo: Función que cuenta los ceros de un número
+# E: Un número entero
+# S: Un número que indica el número de ceros del dígito ingresado
+# R: Números enteros únicamente
+
+def contarCeros (numero):
+    if isinstance (numero, int):
+        if numero == 0:
+            return True
+        elif numero < 0:
+            numero *= -1
+            return contarCeros_aux (numero)
+        else:
+            return contarCeros_aux (numero)
+    else:
+        return "Error, los elementos de entrada son incorrectos"
+
+
+def contarCeros_aux (p_numero):
+    if p_numero == 0:
+        return 0
+    else:
+        if p_numero % 10 == 0:
+            return contarCeros_aux (p_numero // 10) + 1
+        else:
+            return contarCeros_aux (p_numero // 10)
