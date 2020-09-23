e7182e2cc806b196d92f71727e0648b6e2f6c5d6
Gerson2102 <71728860+Gerson2102@users.noreply.github.com>
2020-09-22 22:17:53 -0600

    Archivo python
    Función que determina si un número ingresado es par

---
+++ par.py
@@ -0,0 +1,17 @@
+# Objetivo: Determinar si un número es par
+# E: Un número
+# S: True si es par, False si es impar
+# R: Los números deben ser enteros positivos
+
+def par (numero):
+    if isinstance (numero, int) and (numero >= 0):
+        return par_aux (numero)
+    else:
+        return "Error, el elemento de entrada debe ser un número entero positivo, intente de nuevo por favor"
+
+
+def par_aux (p_numero):
+    if p_numero % 2 == 0:
+        return True
+    else:
+        return False
