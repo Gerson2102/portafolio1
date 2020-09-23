76106484df067e6ba6ed1b82d35a5832dd4f9d53
Gerson2102 <71728860+Gerson2102@users.noreply.github.com>
2020-09-22 22:20:05 -0600

    Archivo python
    Función que determina si un número es impar

---
+++ impar.py
@@ -0,0 +1,17 @@
+# Objetivo: Determinar si un número es impar
+# E: Un número
+# S: True si es impar, False si es par
+# R: Los números deben ser enteros positivos
+
+def impar (numero):
+    if isinstance (numero, int) and (numero >= 0):
+        return impar_aux (numero)
+    else:
+        return "Error, el elemento de entrada debe ser un número entero positivo, intente de nuevo por favor"
+
+
+def impar_aux (p_numero):
+    if p_numero % 2 != 0:
+        return True
+    else:
+        return False
