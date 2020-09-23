6e80eb01c99efd1f1b7c3a599e01904a727426ea
Gerson2102 <71728860+Gerson2102@users.noreply.github.com>
2020-09-22 22:47:05 -0600

    Archivo python
    Función que desarrolla la sumatoria de un número dependiendo de un rango dado

---
+++ sumatoriaRangos.py
@@ -0,0 +1,17 @@
+# Objetivo: Función que desarrolla la sumatoria de un número dependiendo de un rango dado
+# E: Un número de inicio, un número de parada y un rango
+# S: La sumatoria de un número dependiendo de un rango dado por el usuario
+# R: Solo ingresan números enteros
+
+def sumatoriaRangos (numero_inicio, numero_parada, rango):
+    if isinstance (numero_inicio, int) and isinstance (numero_parada, int) and isinstance (rango, int):
+        return sumatoriaRangos_aux (numero_inicio, numero_parada, rango)
+    else:
+        return "Error, los elementos de entrada son incorrectos, deben ser números enteros"
+
+
+def sumatoriaRangos_aux (p_numero_inicio, p_numero_parada, p_rango):
+    if p_numero_inicio > p_numero_parada:
+        return 0
+    else:
+        return sumatoriaRangos_aux (p_numero_inicio + p_rango, p_numero_parada, p_rango) + p_numero_inicio
